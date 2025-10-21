878b6150e9e694cbd4b359caabf958b91e7d3454
enzo.trujillo <enzo.trujillo@dreamsquad.com.br>
2025-10-20 22:45:25 -0300

    opa

--- src/pages/EloSystem.jsx
+++ src/pages/EloSystem.jsx
@@ -79,12 +79,12 @@ const MyEloTab = ({ userProfile, userElo, eloConfig }) => {
   const ELO_CONFIG = {
     ferro: { name: 'Ferro', order: 1, xp: 0, color: '#8B4513', image: '/images/iron.png', description: 'Início da jornada' },
     bronze: { name: 'Bronze', order: 2, xp: 1250, color: '#CD7F32', image: '/images/bronze.png', description: 'Primeiros passos' },
-    prata: { name: 'Prata', order: 3, xp: 5450, color: '#C0C0C0', image: '/images/silver.png', description: 'Crescimento constante' },
-    ouro: { name: 'Ouro', order: 4, xp: 13950, color: '#FFD700', image: '/images/gold.png', description: 'Excelência em atividade' },
-    platina: { name: 'Platina', order: 5, xp: 29300, color: '#E5E4E2', image: '/images/platinum.png', description: 'Dedicação exemplar' },
-    esmeralda: { name: 'Esmeralda', order: 6, xp: 48100, color: '#50C878', image: '/images/emerald.png', description: 'Maestria em engajamento' },
-    diamante: { name: 'Diamante', order: 7, xp: 70400, color: '#B9F2FF', image: '/images/diamond.png', description: 'Elite da plataforma' },
-    mestre: { name: 'Mestre', order: 8, xp: 98600, color: '#800080', image: '/images/master.png', description: 'Lenda da comunidade' }
+    prata: { name: 'Prata', order: 3, xp: 4200, color: '#C0C0C0', image: '/images/silver.png', description: 'Crescimento constante' },
+    ouro: { name: 'Ouro', order: 4, xp: 8500, color: '#FFD700', image: '/images/gold.png', description: 'Excelência em atividade' },
+    platina: { name: 'Platina', order: 5, xp: 15350, color: '#E5E4E2', image: '/images/platinum.png', description: 'Dedicação exemplar' },
+    esmeralda: { name: 'Esmeralda', order: 6, xp: 18800, color: '#50C878', image: '/images/emerald.png', description: 'Maestria em engajamento' },
+    diamante: { name: 'Diamante', order: 7, xp: 22300, color: '#B9F2FF', image: '/images/diamond.png', description: 'Elite da plataforma' },
+    mestre: { name: 'Mestre', order: 8, xp: 28200, color: '#800080', image: '/images/master.png', description: 'Lenda da comunidade' }
   };
 
   const currentXp = userProfile?.stats?.xp || 0;
@@ -286,12 +286,12 @@ const EloListTab = ({ eloConfig }) => {
   const ELO_CONFIG = {
     ferro: { name: 'Ferro', order: 1, xp: 0, color: '#8B4513', image: '/images/iron.png', description: 'Início da jornada' },
     bronze: { name: 'Bronze', order: 2, xp: 1250, color: '#CD7F32', image: '/images/bronze.png', description: 'Primeiros passos' },
-    prata: { name: 'Prata', order: 3, xp: 5450, color: '#C0C0C0', image: '/images/silver.png', description: 'Crescimento constante' },
-    ouro: { name: 'Ouro', order: 4, xp: 13950, color: '#FFD700', image: '/images/gold.png', description: 'Excelência em atividade' },
-    platina: { name: 'Platina', order: 5, xp: 29300, color: '#E5E4E2', image: '/images/platinum.png', description: 'Dedicação exemplar' },
-    esmeralda: { name: 'Esmeralda', order: 6, xp: 48100, color: '#50C878', image: '/images/emerald.png', description: 'Maestria em engajamento' },
-    diamante: { name: 'Diamante', order: 7, xp: 70400, color: '#B9F2FF', image: '/images/diamond.png', description: 'Elite da plataforma' },
-    mestre: { name: 'Mestre', order: 8, xp: 98600, color: '#800080', image: '/images/master.png', description: 'Lenda da comunidade' }
+    prata: { name: 'Prata', order: 3, xp: 4200, color: '#C0C0C0', image: '/images/silver.png', description: 'Crescimento constante' },
+    ouro: { name: 'Ouro', order: 4, xp: 8500, color: '#FFD700', image: '/images/gold.png', description: 'Excelência em atividade' },
+    platina: { name: 'Platina', order: 5, xp: 15350, color: '#E5E4E2', image: '/images/platinum.png', description: 'Dedicação exemplar' },
+    esmeralda: { name: 'Esmeralda', order: 6, xp: 18800, color: '#50C878', image: '/images/emerald.png', description: 'Maestria em engajamento' },
+    diamante: { name: 'Diamante', order: 7, xp: 22300, color: '#B9F2FF', image: '/images/diamond.png', description: 'Elite da plataforma' },
+    mestre: { name: 'Mestre', order: 8, xp: 28200, color: '#800080', image: '/images/master.png', description: 'Lenda da comunidade' }
   };
 
   const eloEntries = Object.entries(ELO_CONFIG).sort((a, b) => a[1].order - b[1].order);
